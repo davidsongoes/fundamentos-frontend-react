93f11fb5618b284c02f87b2c1ae8270906b6c833
Davidson Góes <davidsongoes@gmail.com>
2026-06-16 00:23:34 -0300

    feat(context): :sparkles: implementação inicial do context

---
+++ src/app/nivel-2/page.tsx
@@ -0,0 +1,10 @@
+import { ContadorGlobal } from "../components/ContadorGlobal";
+
+const Page = () => (
+  <div>
+    <h1 className="text-4xl font-bold">Página Nível 2</h1>
+    <ContadorGlobal />
+  </div>
+);
+
+export default Page;
