9c835ffb239da19ca9d1e8772e68f777532ef71e
Davidson Góes <davidsongoes@gmail.com>
2026-07-09 20:22:48 -0300

    chore(API): :wrench: tipando retorno da API

--- src/app/nivel-3/page.tsx
+++ src/app/nivel-3/page.tsx
@@ -1,13 +1,26 @@
 export default async function Page() {
+  type Pokemon = {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: PokemonResults[];
+  };
+
+  type PokemonResults = {
+    name: string;
+    url: string;
+  };
+
   const data = await fetch(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000",
   );
   //   const data = await fetch("https://api.vercel.app/blog");
-  const pokemons = await data.json();
+  const pokemons: Pokemon = await data.json();
   //   const posts = await data.json();
+  console.log(pokemons);
   return (
     <ul>
-      {pokemons.results.map((pokemon: any) => (
+      {pokemons.results.map((pokemon: PokemonResults) => (
         <div style={{ border: "1rem", margin: "1rem" }} key={pokemon.name}>
           <img
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split("/").slice(-2, -1)[0]}.png`}
