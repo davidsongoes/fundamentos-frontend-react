cdf01ac143438bd898cc08288d24ee462f7d0127
Davidson Góes <davidsongoes@gmail.com>
2026-07-11 20:13:37 -0300

    chore(SWR): :wrench: adiciona parâmetro

--- src/app/nivel-3/community-libraries/page.tsx
+++ src/app/nivel-3/community-libraries/page.tsx
@@ -7,6 +7,9 @@ export default function BlogPage() {
   const { data, error, isLoading } = useSWR(
     "https://api.vercel.app/blog",
     fetcher,
+    {
+      errorRetryInterval: 1000, // Retry after 1 second
+    },
   );
 
   if (isLoading) return <div>Loading...</div>;
